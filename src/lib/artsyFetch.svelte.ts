c04bd809bf5c65ee564ea5b69a45c6f1863061d9
Tade Strehk <git@strehk.eu>
2025-01-21 23:34:06 +0100

    refactor: update environment variable imports to use dynamic loading

--- src/lib/artsyFetch.svelte.ts
+++ src/lib/artsyFetch.svelte.ts
@@ -1,14 +1,11 @@
-import { ARTSY_API_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
-export default async function artsyFetch<T>(
-	path: string,
-	token: string
-): Promise<T> {
-	const res = await fetch(`${ARTSY_API_URL}/api/${path}`, {
+export default async function artsyFetch<T>(path: string, token: string): Promise<T> {
+	const res = await fetch(`${env.ARTSY_API_URL}/api/${path}`, {
 		headers: {
 			'X-Xapp-Token': token,
 			'Content-Type': 'application/json'
-		},
+		}
 	});
 	const data = await res.json();
 	return data;
--- src/routes/gallery/+layout.server.ts
+++ src/routes/gallery/+layout.server.ts
@@ -1,13 +1,13 @@
 import type { LayoutServerLoad } from './$types';
-import { ARTSY_API_URL, ARTSY_API_CLIENT_ID, ARTSY_API_CLIENT_SECRET } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import artsyXapp from '@artsy/xapp';
 
 export const load: LayoutServerLoad = async ({ params }) => {
 	artsyXapp.init(
 		{
-			url: ARTSY_API_URL, // defaults to process.env.ARTSY_URL
-			id: ARTSY_API_CLIENT_ID,
-			secret: ARTSY_API_CLIENT_SECRET
+			url: env.ARTSY_API_URL, // defaults to process.env.ARTSY_URL
+			id: env.ARTSY_API_CLIENT_ID,
+			secret: env.ARTSY_API_CLIENT_SECRET
 		},
 		function () {}
 	);
