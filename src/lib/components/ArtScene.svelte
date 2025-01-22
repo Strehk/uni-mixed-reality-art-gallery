dd12dabbd04728f4d852490c58ce3457534684a7
Tade Strehk <git@strehk.eu>
2025-01-22 15:46:03 +0100

    feat: refactor ArtScene component to optimize lighting and remove hand tracking controls

--- src/lib/components/ArtScene.svelte
+++ src/lib/components/ArtScene.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
 	import { Controller, Hand, useXR, XR } from '@threlte/xr';
-	import { useTexture } from '@threlte/extras';
 	import Wall from './Wall.svelte';
 	import Artwork from './Artwork.svelte';
 	import { calcOptimalPictureHeight } from '$lib/calculations';
-	import { pointerControls } from '@threlte/xr';
-	import { lightTargetPosition } from 'three/tsl';
 
 	interface Props {
 		paintingTextureHref: string;
@@ -18,17 +15,12 @@
 
 	let wallHeight = $derived(calcOptimalPictureHeight(height) + height / 2 + 0.5);
 	let wallWidth = $derived(width + 1);
-
-	const { isHandTracking } = useXR();
-
-	pointerControls('left');
-	pointerControls('right');
 </script>
 
 <T.AmbientLight position={[-10, 10, 5]} />
 <T.AmbientLight position={[10, 10, 5]} />
 
-<T.SpotLight position={[0, 3.5, -2.7]} intensity={100} />
+<T.SpotLight position={[0, calcOptimalPictureHeight(height) + height + 0.5, -2.7]} intensity={100} penumbra={0.3} />
 <Wall width={wallWidth} height={wallHeight} />
 <Artwork {width} {height} {paintingTextureHref} />
 
@@ -42,10 +34,5 @@
 	{/snippet}
 </XR>
 
-{#if isHandTracking}
-	<Hand left />
-	<Hand right />
-{:else}
 	<Controller left />
 	<Controller right />
-{/if}
